0384cb0e88fd4cd8aef48bb39a6f53f91a0eea3a
kodowicz <annakodowicz@gmail.com>
2018-11-29 12:31:59 +0100

    Updates

---
+++ precache-manifest.fa7433e260eec395f55fbea2a8097402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sentences/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bd76a13870e40c9513ef",
+    "url": "/sentences/static/js/runtime~main.bd76a138.js"
+  },
+  {
+    "revision": "821d8ed9d331c06385ca",
+    "url": "/sentences/static/js/main.821d8ed9.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/sentences/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "821d8ed9d331c06385ca",
+    "url": "/sentences/static/css/main.a3973e04.chunk.css"
+  },
+  {
+    "revision": "6daea0c7fcd95e73effa6d954869d790",
+    "url": "/sentences/index.html"
+  }
+];
