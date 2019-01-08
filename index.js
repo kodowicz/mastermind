5061f3ef0d2eb2356ac3aac2613b710446fb21e6
kodowicz <annakodowicz@gmail.com>
2019-01-08 10:18:11 +0100

    ball's movement

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+let lastTargetSlot;
 
+function onDragStart(event) {
+  event.dataTransfer.effectAllowed='move';
+  event.dataTransfer.setData("text", event.target.getAttribute('id'));
+
+  if (event.target.parentNode.classList.contains('slot')) {
+    lastTargetSlot = event.target.parentNode;
+  }
+
+  return true;
+}
+
+
+function onDrop(event) {
+  const data = event.dataTransfer.getData("text");
+  event.target.appendChild(document.getElementById(data));
+
+  if (event.target.classList.contains('ball')) {
+    event.target.parentNode.appendChild(document.getElementById(data));
+    lastTargetSlot.appendChild(event.target);
+    console.log(event.target);
+  }
+
+  event.stopPropagation();
+  return false;
+}
+
+function onDragOver(event) {
+  event.preventDefault();
+}
+
+function onDragEnd(event) {
+}
