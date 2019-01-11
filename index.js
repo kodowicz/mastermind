462fc5851b3aa72a2a5a990cadf51166245c0c08
kodowicz <annakodowicz@gmail.com>
2019-01-11 18:11:16 +0100

    ball's movement

--- index.js
+++ index.js
@@ -5,33 +5,44 @@ let createChild;
 let over;
 let dragStart;
 
+function removeNumbers(name) {
+  let changedName;
+  let rex = name.search(/\d/g);
+
+  if (rex == -1) {
+    changedName = name;
+  } else {
+    changedName = name.slice(0, rex);
+  }
+
+  return changedName;
+}
+
 function findIndex(id) {
-  let name = id.replace(id[id.length - 1], '');
+  let name = removeNumbers(id);
   const elements = document.querySelectorAll(`.${name}`);
-  let index = 1;
-  console.log(elements);
+  let index = 0;
 
   elements.forEach(element => {
     let match = element.id.match(/\d/g);
-    console.log(match);
+
     if (match !== null) {
       match = parseInt(match[0], 10);
-      //if (match > index) {
-        index = match + 1;
-        console.log('index');
-      //}
-    } else {
-      index = 1;
-      console.log(index);
-    }
-
+      if (match > index) {
+        index = match;
+      } else {
+        return;
+      };
+    };
   });
-  return index;
+  return index + 1;
 }
 
 function createElement(className, idName) {
-  let index = findIndex(idName);
   let element = document.createElement('div');
+  let index = findIndex(idName);
+  idName = removeNumbers(idName);
+
   element.className = className;
   element.id = `${idName}${index}`;
   element.setAttribute('draggable', 'true');
@@ -59,7 +70,6 @@ function onDragStart(event) {
     lastTargetSlot = event.target.parentNode;
     console.log(lastTargetSlot)*/
   }
-
   return true;
 }
 
@@ -69,69 +79,86 @@ function onDrop(event) {
   let targetParent = event.target.parentNode;
 
 
-  // przeniesienie kulki z slot do colors
-  if (createChild.parentNode.className === 'color-slot') {
-    //console.log('slot -> placeholder');
-    const newElement = createElement(
-      createChild.className,
-      createChild.id
-    );
-    //createChild.parentNode.appendChild(newElement);
-    //event.target.appendChild(document.getElementById(data));
+  // ball w to samo miejsce gdzie było
+  if (lastTargetSlot === targetParent) {
+    targetParent.appendChild(document.getElementById(data));
+  } else {
 
-    //console.log(event.target);
-    // zamiana kuli z placeholder do zajętego slot
-    if (event.target.classList.contains('ball')) {
+    if (targetParent.className === 'color-slot') {
+
+      // kulka na placeholder
+      if (lastTargetSlot.className === 'slot') {
+        const newElement = createElement(event.target.className, event.target.id);
+        lastTargetSlot.removeChild(createChild);
+        lastTargetSlot.appendChild(newElement);
+
+        return;
+      } else {
+        // placeholder na placeholder
+        return;
+      }
+    }
+
+  // przeniesienie kulki z slot do colors
+    if (createChild.parentNode.className === 'color-slot') {
+      //console.log('slot -> placeholder');
+      const newElement = createElement(createChild.className, createChild.id);
+      //createChild.parentNode.appendChild(newElement);
+      //event.target.appendChild(document.getElementById(data));
+
+
+      // zamiana kuli z placeholder do zajętego slot
+      if (event.target.classList.contains('ball')) {
+        targetParent.removeChild(event.target);
+        targetParent.appendChild(document.getElementById(data));
+        lastTargetSlot.appendChild(newElement);
+      } else {
+        createChild.parentNode.appendChild(newElement);
+        event.target.appendChild(document.getElementById(data));
+      }
+      return;
+    }
+
+
+    // przeniesienie kulki z slot do slot
+    if (event.target.classList.contains('ball') &&
+        createChild.parentNode.parentNode === over.parentNode.parentNode) {
+      //console.log('slot -> slot');
+
+
+      //event.target.parentNode.appendChild(document.getElementById(data));
+      console.log('t');
       targetParent.removeChild(event.target);
       targetParent.appendChild(document.getElementById(data));
+      lastTargetSlot.append(event.target);
+
+      /*parent.removeChild(event.target);
+      //lastTargetSlot.append(event.target);
+      parent.appendChild(document.getElementById(data));
+      console.log('b')
+      const newElement = createElement(
+        'div',
+        createChild.className,
+        createChild.id
+      );
       lastTargetSlot.appendChild(newElement);
-    } else {
-      createChild.parentNode.appendChild(newElement);
-      event.target.appendChild(document.getElementById(data));
-    }
+  */
     return;
+    // przeniesienie slot do pustego slot
+    } else if (!event.target.classList.contains('ball') && createChild.parentNode.parentNode === over.parentNode.parentNode) {
+      event.target.appendChild(document.getElementById(data));
+      //console.log('slot -> empty slot');
+    // przejście z colors do pustego slota
+    } else {
+      /*const newElement = createElement(
+        'div',
+        createChild.className,
+        createChild.id
+      );
+      console.log('else');
+      event.target.appendChild(document.getElementById(data));*/
+    }
   }
-
-
-  // przeniesienie kulki z slot do slot
-  if (event.target.classList.contains('ball') &&
-      createChild.parentNode.parentNode === over.parentNode.parentNode) {
-    //console.log('slot -> slot');
-
-
-    //event.target.parentNode.appendChild(document.getElementById(data));
-
-    targetParent.removeChild(event.target);
-    targetParent.appendChild(document.getElementById(data));
-    lastTargetSlot.append(event.target);
-
-    /*parent.removeChild(event.target);
-    //lastTargetSlot.append(event.target);
-    parent.appendChild(document.getElementById(data));
-    console.log('b')
-    const newElement = createElement(
-      'div',
-      createChild.className,
-      createChild.id
-    );
-    lastTargetSlot.appendChild(newElement);
-*/
-  return;
-  // przeniesienie slot do pustego slot
-  } else if (!event.target.classList.contains('ball') && createChild.parentNode.parentNode === over.parentNode.parentNode) {
-    event.target.appendChild(document.getElementById(data));
-    //console.log('slot -> empty slot');
-  // przejście z colors do pustego slota
-  } else {
-    /*const newElement = createElement(
-      'div',
-      createChild.className,
-      createChild.id
-    );
-    console.log('else');
-    event.target.appendChild(document.getElementById(data));*/
-  }
-
 
   event.stopPropagation();
   return false;
